5802e53ae426293f0e5fbde0d76a1c7fc47d41b9
mirandaweston <mweston105@yahoo.co.uk>
2023-04-21 02:07:59 +0100

    Adding stocks to HomePage: passes tests

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,9 +10,48 @@
           <th>Change</th>
         </tr>
       </thead>
+      <tbody>
+        <tr v-for="stock in stocks" :key="stock.ticker">
+          <td>{{ stock.name }}</td>
+          <td>{{ stock.ticker }}</td>
+          <td>{{ stock.price }}</td>
+          <td>{{ stock.change }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      stocks: [
+        { name: "Apple Inc.", ticker: "AAPL", price: 135.75, change: -0.25 },
+        {
+          name: "Microsoft Corporation",
+          ticker: "MSFT",
+          price: 255.85,
+          change: 1.2,
+        },
+        {
+          name: "Amazon.com, Inc.",
+          ticker: "AMZN",
+          price: 3312.0,
+          change: -5.5,
+        },
+        { name: "Tesla, Inc.", ticker: "TSLA", price: 725.25, change: 2.1 },
+        {
+          name: "Alphabet Inc.",
+          ticker: "GOOGL",
+          price: 2267.5,
+          change: -0.75,
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 .home-page {
--- tests/HomePage.cy.js
+++ tests/HomePage.cy.js
@@ -1,12 +1,72 @@
 import HomePage from "../src/components/HomePage";
 
 describe("HomePage", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      stocks: [
+        { name: "Apple Inc.", ticker: "AAPL", price: 135.75, change: -0.25 },
+        {
+          name: "Microsoft Corporation",
+          ticker: "MSFT",
+          price: 255.85,
+          change: 1.2,
+        },
+        {
+          name: "Amazon.com, Inc.",
+          ticker: "AMZN",
+          price: 3312.0,
+          change: -5.5,
+        },
+        { name: "Tesla, Inc.", ticker: "TSLA", price: 725.25, change: 2.1 },
+        {
+          name: "Alphabet Inc.",
+          ticker: "GOOGL",
+          price: 2267.5,
+          change: -0.75,
+        },
+      ],
+    };
+
+    cy.mount(HomePage, { props });
+  });
+
   it("displays all headings", () => {
-    cy.mount(HomePage);
     cy.get("h1").should("contain", "Stocks");
     cy.get("table th").should("contain", "Name");
     cy.get("table th").should("contain", "Ticker");
     cy.get("table th").should("contain", "Price");
     cy.get("table th").should("contain", "Change");
   });
+
+  it("displays all stocks", () => {
+    cy.get("table tbody tr").should("have.length", stocks.length);
+    stocks.forEach((stock, index) => {
+      cy.get(`table tbody tr:nth-child(${index + 1}) td:nth-child(1)`).should(
+        "contain",
+        stock.name
+      );
+      cy.get(`table tbody tr:nth-child(${index + 1}) td:nth-child(2)`).should(
+        "contain",
+        stock.ticker
+      );
+      cy.get(`table tbody tr:nth-child(${index + 1}) td:nth-child(3)`).should(
+        "contain",
+        stock.price
+      );
+      cy.get(`table tbody tr:nth-child(${index + 1}) td:nth-child(4)`).should(
+        "contain",
+        stock.change
+      );
+    });
+  });
 });
+
+const stocks = [
+  { name: "Apple Inc.", ticker: "AAPL", price: 135.75, change: -0.25 },
+  { name: "Microsoft Corporation", ticker: "MSFT", price: 255.85, change: 1.2 },
+  { name: "Amazon.com, Inc.", ticker: "AMZN", price: 3312.0, change: -5.5 },
+  { name: "Tesla, Inc.", ticker: "TSLA", price: 725.25, change: 2.1 },
+  { name: "Alphabet Inc.", ticker: "GOOGL", price: 2267.5, change: -0.75 },
+];
